7beced1850c1c2e27a96de10f4205be6688eb195
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-29 17:59:38 +0200

    Renamed ImageBoxComposable to SkillGrid.

--- app/src/main/java/com/koleff/habittracker/ui/composable/SkillGrid.kt
+++ app/src/main/java/com/koleff/habittracker/ui/composable/SkillGrid.kt
@@ -3,21 +3,14 @@ package com.koleff.habittracker.ui.composable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -34,7 +27,6 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.koleff.habittracker.domain.Skill
-import com.koleff.habittracker.ui.SkillCard
 
 @Composable
 fun ImageCard(
