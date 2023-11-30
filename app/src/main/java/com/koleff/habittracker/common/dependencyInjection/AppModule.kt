6007768da96cd6d51cd8329c9bbb76e0d53a6129
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-30 13:25:48 +0200

    Create AppModule.kt

---
+++ app/src/main/java/com/koleff/habittracker/common/dependencyInjection/AppModule.kt
@@ -0,0 +1,28 @@
+package com.koleff.habittracker.common.dependencyInjection
+
+import com.koleff.habittracker.data.repository.FakeSkillRepositoryImpl
+import com.koleff.habittracker.domain.repository.SkillRepository
+import com.koleff.habittracker.ui.viewModel.SkillListViewModel
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFakeSkillRepositoryImpl(): SkillRepository {
+        return FakeSkillRepositoryImpl()
+    }
+
+
+    @Provides
+    @Singleton
+    fun providesSkillListViewModel(skillRepository: SkillRepository): SkillListViewModel{
+        return SkillListViewModel(skillRepository)
+    }
+}
