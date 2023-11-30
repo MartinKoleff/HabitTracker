6928d1139aa0d2bd522bddc2763e3bc28c76ed25
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-30 14:02:26 +0200

    Wired BottomNavigationBar with NavController.

--- app/src/main/java/com/koleff/habittracker/ui/composable/BottomNavigationBar.kt
+++ app/src/main/java/com/koleff/habittracker/ui/composable/BottomNavigationBar.kt
@@ -11,26 +11,46 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavHostController
+import com.koleff.habittracker.data.MainScreen
 
 @Composable
-fun BottomNavigationBar() {
+fun BottomNavigationBar(navController: NavHostController) {
     BottomAppBar(
         actions = {
-            IconButton(onClick = { /* do something */ }) {
-                Icon(Icons.Filled.Star, contentDescription = "Dashboard")
-            }
-            FloatingActionButton(onClick = { /* do something */ }) {
+            NavigationItem(
+                navController = navController,
+                screen = MainScreen.Dashboard,
+                icon = Icons.Filled.Star,
+                label = "Dashboard"
+            )
+            FloatingActionButton(onClick = { navController.navigate(MainScreen.Search.route) }) {
                 Icon(
                     Icons.Filled.Search,
                     contentDescription = "Search skill",
                 )
             }
-            IconButton(onClick = { /* do something */ }) {
-                Icon(
-                    Icons.Filled.Add,
-                    contentDescription = "Add new skill",
-                )
-            }
+            NavigationItem(
+                navController = navController,
+                screen = MainScreen.Add,
+                icon = Icons.Filled.Add,
+                label = "Add new skill"
+            )
         }
     )
+}
+
+@Composable
+fun NavigationItem(
+    navController: NavHostController,
+    screen: MainScreen,
+    icon: ImageVector,
+    label: String
+) {
+    IconButton(
+        onClick = { navController.navigate(screen.route) }
+    ) {
+        Icon(imageVector = icon, contentDescription = label)
+    }
 }
