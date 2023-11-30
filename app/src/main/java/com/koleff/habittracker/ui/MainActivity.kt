295cc7198bdefe0bf45a44bdad21bd571bbbf3b7
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-30 13:25:05 +0200

    Moved all dashboard logic from activity to dashboard screen.

--- app/src/main/java/com/koleff/habittracker/ui/MainActivity.kt
+++ app/src/main/java/com/koleff/habittracker/ui/MainActivity.kt
@@ -3,85 +3,36 @@ package com.koleff.habittracker.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.koleff.habittracker.R
-import com.koleff.habittracker.domain.Skill
-import com.koleff.habittracker.domain.SkillCategory
-import com.koleff.habittracker.domain.SkillType
-import com.koleff.habittracker.ui.composable.BottomNavigationBar
-import com.koleff.habittracker.ui.composable.SkillGrid
-import com.koleff.habittracker.ui.composable.TopToolbar
-import com.koleff.habittracker.ui.theme.HabitTrackerTheme
-import com.koleff.habittracker.ui.viewModel.SkillViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.koleff.habittracker.data.MainScreen
+import com.koleff.habittracker.ui.composable.screen.DashboardScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val viewModel: SkillViewModel by viewModels() //Used for MyComposableActivity
-
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val skillList: List<Skill> = mockupSkills()
 
         setContent {
-            HabitTrackerTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    topBar = {
-                        TopToolbar()
-                    },
-                    bottomBar = {
-                        BottomNavigationBar()
-                    }
-                ) { innerPadding ->
-                    SkillGrid(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize(),
-                        skillList = skillList
-                    )
-                }
+            val navController = rememberNavController()
 
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    MyComposableActivity(viewModel)
-//                }
+            NavHost(
+                navController = navController,
+                startDestination = MainScreen.Dashboard.route,
+                modifier = Modifier.padding()
+            ) {
+                composable(MainScreen.Dashboard.route) { DashboardScreen() }
+//                composable(MainScreen.Search.route) { SearchScreen() }
+//                composable(MainScreen.Add.route) { AddSkillScreen() }
             }
         }
-    }
-
-    private fun mockupSkills(): List<Skill> {
-        val skillList = mutableListOf<Skill>()
-        var skill: Skill
-        val n = 27
-
-        repeat(n) {
-            skill = Skill(
-                name = "Jetpack Compose $it",
-                description = "Learn Jetpack Compose framework in Android",
-                imageId = R.drawable.jetpack_compose,
-                type = SkillType.COMPLETABLE,
-                category = SkillCategory.LEARNING
-            )
-            skillList.add(skill)
-        }
-
-        return skillList
     }
 }
 
--- app/src/main/java/com/koleff/habittracker/ui/composable/screen/DashboardScreen.kt
+++ app/src/main/java/com/koleff/habittracker/ui/composable/screen/DashboardScreen.kt
@@ -5,14 +5,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.koleff.habittracker.ui.composable.BottomNavigationBar
 import com.koleff.habittracker.ui.composable.SkillGrid
 import com.koleff.habittracker.ui.composable.TopToolbar
+import com.koleff.habittracker.ui.viewModel.SkillListViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DashboardScreen() {
+fun DashboardScreen(
+    skillListViewModel: SkillListViewModel = hiltViewModel()
+) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -22,11 +30,13 @@ fun DashboardScreen() {
             BottomNavigationBar()
         }
     ) { innerPadding ->
+        val data = skillListViewModel.skills.collectAsState()
+
         SkillGrid(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize(),
-            skillList = skillList
+            skillList = data.value ?: listOf()
         )
     }
 }
