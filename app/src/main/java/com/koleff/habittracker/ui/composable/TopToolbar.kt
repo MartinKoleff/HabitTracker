3b351b03f21371d1d7dc7a6e871330904eb914d4
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-03 13:05:39 +0200

    Wired TopToolbar with NavigationDrawer. Added new button composable.

--- app/src/main/java/com/koleff/habittracker/ui/composable/TopToolbar.kt
+++ app/src/main/java/com/koleff/habittracker/ui/composable/TopToolbar.kt
@@ -1,10 +1,14 @@
 package com.koleff.habittracker.ui.composable
 
+import android.graphics.drawable.Icon
+import android.util.Log
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -13,14 +17,21 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.koleff.habittracker.data.MainScreen
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopToolbar(navController: NavHostController, title: String) {
+fun TopToolbar(
+    navController: NavHostController,
+    title: String,
+    onNavigationDrawerIconClick: () -> Unit
+) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
 
     CenterAlignedTopAppBar(
@@ -44,14 +55,22 @@ fun TopToolbar(navController: NavHostController, title: String) {
             )
         },
         actions = {
-            NavigationItem(
-                navController = navController,
-                screen = null, //Backstack pop
+            NavigationDrawerButton(
                 icon = Icons.Filled.Menu,
-                label = "Options menu drawer"
+                label = "Options menu drawer",
+                onClick = onNavigationDrawerIconClick
             )
         },
         scrollBehavior = scrollBehavior
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationDrawerButton(icon: ImageVector, label: String, onClick: () -> Unit) {
+    IconButton(
+        onClick = onClick
+    ) {
+        Icon(imageVector = icon, contentDescription = label)
+    }
+}
