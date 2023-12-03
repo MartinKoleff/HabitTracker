8713e1e6c3b22def0458530abca8755a9da5355a
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-03 13:15:01 +0200

    Main screens imports cleanup.

--- app/src/main/java/com/koleff/habittracker/ui/composable/screen/DashboardScreen.kt
+++ app/src/main/java/com/koleff/habittracker/ui/composable/screen/DashboardScreen.kt
@@ -2,25 +2,15 @@ package com.koleff.habittracker.ui.composable.screen
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-import androidx.navigation.navArgument
-import com.koleff.habittracker.ui.composable.BottomNavigationBar
 import com.koleff.habittracker.ui.composable.MainScreenScaffold
 import com.koleff.habittracker.ui.composable.SkillGrid
-import com.koleff.habittracker.ui.composable.TopToolbar
 import com.koleff.habittracker.ui.viewModel.SkillListViewModel
-import dagger.hilt.android.AndroidEntryPoint
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreen(
     navController: NavHostController,
--- app/src/main/java/com/koleff/habittracker/ui/composable/screen/SearchScreen.kt
+++ app/src/main/java/com/koleff/habittracker/ui/composable/screen/SearchScreen.kt
@@ -1,16 +1,9 @@
 package com.koleff.habittracker.ui.composable.screen
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import com.koleff.habittracker.ui.composable.BottomNavigationBar
 import com.koleff.habittracker.ui.composable.MainScreenScaffold
-import com.koleff.habittracker.ui.composable.TopToolbar
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(navController: NavHostController) {
     MainScreenScaffold("Search", navController) {
