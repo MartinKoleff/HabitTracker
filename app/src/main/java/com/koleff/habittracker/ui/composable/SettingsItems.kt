43ec5ab661d5f1460fe4dbb16677d8cb39ad870a
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-03 16:47:09 +0200

    Added switch button for biometrics option in settings.
    Added arrow for all other options.

--- app/src/main/java/com/koleff/habittracker/ui/composable/SettingsItems.kt
+++ app/src/main/java/com/koleff/habittracker/ui/composable/SettingsItems.kt
@@ -1,38 +1,40 @@
 package com.koleff.habittracker.ui.composable
 
-import android.graphics.drawable.Icon
-import android.provider.Settings
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.ExitToApp
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Place
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.rounded.AccountCircle
-import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import com.koleff.habittracker.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingsListItem(title: String, icon: ImageVector, description: String) {
+fun SettingsListItem(
+    title: String,
+    icon: ImageVector,
+    description: String,
+    hasSwitch: Boolean = false
+) {
     ListItem(
         headlineContent = { Text(title) },
         leadingContent = {
@@ -40,13 +42,28 @@ fun SettingsListItem(title: String, icon: ImageVector, description: String) {
                 icon,
                 contentDescription = description,
             )
+        },
+        trailingContent = {
+            if (hasSwitch) {
+                SwitchButton()
+            }else{
+//                Icon(
+//                    painterResource(id = R.drawable.ic_vector_back),
+//                    contentDescription = "Go inside",
+//                )
+            }
         }
     )
     HorizontalDivider()
 }
 
 @Composable
-fun SettingsListItem(title: String, icon: Painter, description: String) {
+fun SettingsListItem(
+    title: String,
+    icon: Painter,
+    description: String,
+    hasSwitch: Boolean = false
+) {
     ListItem(
         headlineContent = { Text(title) },
         leadingContent = {
@@ -54,6 +71,16 @@ fun SettingsListItem(title: String, icon: Painter, description: String) {
                 icon,
                 contentDescription = description,
             )
+        },
+        trailingContent = {
+            if (hasSwitch) {
+                SwitchButton()
+            }else{
+//                Icon(
+//                    painterResource(id = R.drawable.ic_vector_back),
+//                    contentDescription = "Go inside",
+//                )
+            }
         }
     )
     HorizontalDivider()
@@ -65,6 +92,20 @@ fun SettingsCategory(title: String) {
 }
 
 @Composable
+fun SwitchButton() {
+    var checked by remember { mutableStateOf(true) }
+
+    Switch(
+        checked = checked,
+        onCheckedChange = {
+            checked = it
+
+            //TODO: add biometrics authentication dialog / screen logic...
+        }
+    )
+}
+
+@Composable
 fun SettingsList(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         SettingsListItem("Logout", painterResource(R.drawable.ic_vector_logout), "Logout from account")
@@ -72,8 +113,8 @@ fun SettingsList(modifier: Modifier = Modifier) {
         SettingsListItem("Change password", painterResource(R.drawable.ic_vector_password), "Change password")
 
         SettingsListItem("Push notifications", Icons.Default.Notifications, "Push notifications settings")
-        SettingsListItem("Change language", painterResource(R.drawable.ic_vector_language), "Change language") //TODO: add switcher view
-        SettingsListItem("Biometric authentication", painterResource(R.drawable.ic_faceid), "Biometric authentication")
+        SettingsListItem("Change language", painterResource(R.drawable.ic_vector_language), "Change language")
+        SettingsListItem("Biometric authentication", painterResource(R.drawable.ic_faceid), "Biometric authentication", true)
 
         SettingsCategory("Privacy policy")
         SettingsListItem("Privacy policy", Icons.Default.Info, "Privacy policy")
