cca141860b935ffc2bc426ee166ea74fb4cdf037
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-03 15:51:57 +0200

    Added Settings List wired with screen.

--- app/src/main/java/com/koleff/habittracker/ui/composable/screen/SettingsScreen.kt
+++ app/src/main/java/com/koleff/habittracker/ui/composable/screen/SettingsScreen.kt
@@ -1,13 +1,22 @@
 package com.koleff.habittracker.ui.composable.screen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import com.koleff.habittracker.data.MainScreen
 import com.koleff.habittracker.ui.composable.MainScreenScaffold
+import com.koleff.habittracker.ui.composable.SettingsList
 
 @Composable
 fun SettingsScreen(navController: NavHostController) {
-    MainScreenScaffold("Settings", navController) {
+    MainScreenScaffold("Settings", navController) { innerPadding ->
 
+        SettingsList(
+            Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        )
     }
 }
