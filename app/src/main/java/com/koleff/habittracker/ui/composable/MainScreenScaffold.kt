3f2f8bde2dda3c99ce38833608095ab34d8b1f70
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-03 13:06:07 +0200

    Wired MainScreen Scaffold with NavigationDrawer.

--- app/src/main/java/com/koleff/habittracker/ui/composable/MainScreenScaffold.kt
+++ app/src/main/java/com/koleff/habittracker/ui/composable/MainScreenScaffold.kt
@@ -2,11 +2,19 @@ package com.koleff.habittracker.ui.composable
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -15,15 +23,30 @@ fun MainScreenScaffold(
     navController: NavHostController,
     content: @Composable (paddingValues: PaddingValues) -> Unit
 ) {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            TopToolbar(navController = navController, title = screenTitle)
-        },
-        bottomBar = {
-            BottomNavigationBar(navController = navController)
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(initialDrawerState = drawerState) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopToolbar(
+                    navController = navController,
+                    title = screenTitle,
+                    onNavigationDrawerIconClick = {
+                        scope.launch {
+                            drawerState.apply {
+                                if (isClosed) open() else close()
+                            }
+                        }
+                    }
+                )
+            },
+            bottomBar = {
+                BottomNavigationBar(navController = navController)
+            }
+        ) { innerPadding ->
+            content(innerPadding)
         }
-    ) { innerPadding ->
-        content(innerPadding)
     }
 }
