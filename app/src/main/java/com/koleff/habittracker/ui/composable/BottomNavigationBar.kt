39f14045f477fb17b715a6256dd3dbab9aa2ae6c
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-03 14:19:20 +0200

    Added text to Bottom Navigation icons.
    I want to add effect of when button is pressed its text appears. The background box becomes larger.

--- app/src/main/java/com/koleff/habittracker/ui/composable/BottomNavigationBar.kt
+++ app/src/main/java/com/koleff/habittracker/ui/composable/BottomNavigationBar.kt
@@ -1,21 +1,12 @@
 package com.koleff.habittracker.ui.composable
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.koleff.habittracker.data.MainScreen
 
@@ -24,37 +15,41 @@ fun BottomNavigationBar(navController: NavHostController) {
     BottomAppBar(
         actions = {
             NavigationItem(
+                modifier = Modifier.weight(1f),
                 navController = navController,
                 screen = MainScreen.Dashboard,
                 icon = Icons.Filled.Star,
                 label = "Dashboard"
             )
 
-            //Spacing between items
-            Spacer(
-                modifier = Modifier
-                    .weight(1f)
-                    .width(0.dp)
-            )
+//            //Spacing between items
+//            Spacer(
+//                modifier = Modifier
+//                    .weight(1f)
+//                    .width(0.dp)
+//            )
 
-            FloatingNavigationItem(
+            NavigationItem(
+                modifier = Modifier.weight(0.5f),
                 navController = navController,
                 screen = MainScreen.Search,
                 icon = Icons.Filled.Search,
                 label = "Search skill"
             )
 
-            //Spacing between items
-            Spacer(
-                modifier = Modifier
-                    .weight(1f)
-                    .width(0.dp)
-            )
+//            //Spacing between items
+//            Spacer(
+//                modifier = Modifier
+//                    .weight(1f)
+//                    .width(0.dp)
+//            )
+
             NavigationItem(
+                modifier = Modifier.weight(1f),
                 navController = navController,
-                screen = MainScreen.Add,
-                icon = Icons.Filled.Add,
-                label = "Add new skill"
+                screen = MainScreen.Settings,
+                icon = Icons.Filled.Settings,
+                label = "Settings"
             )
         }
     )
