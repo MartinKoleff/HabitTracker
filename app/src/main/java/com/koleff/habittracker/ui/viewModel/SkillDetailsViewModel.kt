c815c4927c62e52368c37f4dd45b96a6bcd46870
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-07 22:35:34 +0200

    Added assisted inject for SkillDetailsViewModel.
    When providing a component via dependency injection you need to inject all dependencies. In SkillDetailsViewModel the repository is provided and skillId is assisted injected but dispatcher is not.

--- app/src/main/java/com/koleff/habittracker/ui/viewModel/SkillDetailsViewModel.kt
+++ app/src/main/java/com/koleff/habittracker/ui/viewModel/SkillDetailsViewModel.kt
@@ -5,19 +5,21 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.koleff.habittracker.data.Skill
 import com.koleff.habittracker.domain.repository.SkillRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-@HiltViewModel
-class SkillDetailsViewModel @Inject constructor(
+class SkillDetailsViewModel @AssistedInject constructor(
     private val skillRepository: SkillRepository,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+    @Assisted private val skillId: Int,
+    @Assisted private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
     private var _skill: MutableState<Skill?> = mutableStateOf(null)
@@ -26,10 +28,29 @@ class SkillDetailsViewModel @Inject constructor(
     fun getSkill(id: Int) {
         _skill.value = null
         Log.d("SkillDetailsViewModel", "Cleared previous state for id: $id")
+    init {
+        getSkill(id = skillId)
+    }
 
         viewModelScope.launch(dispatcher) {
             _skill.value = skillRepository.getSkill(id)
             Log.d("SkillDetailsViewModel", "Fetched new data for id: $id")
+
+    @AssistedFactory
+    interface Factory {
+        fun create(skillId: Int, dispatcher: CoroutineDispatcher): SkillDetailsViewModel
+    }
+    companion object {
+        fun provideExerciseViewModelFactory(
+            factory: Factory,
+            skillId: Int,
+            dispatcher: CoroutineDispatcher = Dispatchers.Main
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return factory.create(skillId, dispatcher) as T
+            }
         }
     }
 }
+
+
