37d0c5865ab14089de5310856c6e91725335d5b2
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-07 22:36:22 +0200

    Injecting skillDetailsViewModelFactory in MainActivity. Wiring SetupNavGraph with factory.

--- app/src/main/java/com/koleff/habittracker/ui/MainActivity.kt
+++ app/src/main/java/com/koleff/habittracker/ui/MainActivity.kt
@@ -3,30 +3,32 @@ package com.koleff.habittracker.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
-import com.koleff.habittracker.data.MainScreen
-import com.koleff.habittracker.ui.composable.SkillDetailsScreen
 import com.koleff.habittracker.ui.composable.navigation.SetupNavGraph
-import com.koleff.habittracker.ui.composable.screen.AddSkillScreen
-import com.koleff.habittracker.ui.composable.screen.DashboardScreen
-import com.koleff.habittracker.ui.composable.screen.SearchScreen
-import com.koleff.habittracker.ui.composable.screen.SettingsScreen
+import com.koleff.habittracker.ui.viewModel.SkillDetailsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject lateinit var skillDetailsViewModelFactory: SkillDetailsViewModel.Factory
+
     @OptIn(ExperimentalComposeUiApi::class, ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+//        val skillDetailsViewModel by viewModels<SkillDetailsViewModel> {
+//            SkillDetailsViewModel.provideExerciseViewModelFactory(factory = skillDetailsViewModelFactory, skillId = selectedSkillId)
+//        }
 
         setContent {
             val navController = rememberNavController()
-            SetupNavGraph(navController = navController)
+            SetupNavGraph(navController = navController, skillDetailsViewModelFactory = skillDetailsViewModelFactory)
         }
     }
 }
--- app/src/main/java/com/koleff/habittracker/ui/composable/navigation/SetupNavGraph.kt
+++ app/src/main/java/com/koleff/habittracker/ui/composable/navigation/SetupNavGraph.kt
@@ -3,6 +3,7 @@ package com.koleff.habittracker.ui.composable.navigation
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -12,14 +13,15 @@ import com.koleff.habittracker.ui.composable.screen.AddSkillScreen
 import com.koleff.habittracker.ui.composable.screen.DashboardScreen
 import com.koleff.habittracker.ui.composable.screen.SearchScreen
 import com.koleff.habittracker.ui.composable.screen.SettingsScreen
+import com.koleff.habittracker.ui.viewModel.SkillDetailsViewModel
 
 @ExperimentalComposeUiApi
 @ExperimentalAnimationApi
 @Composable
 fun SetupNavGraph(
-    navController: NavHostController
+    navController: NavHostController,
+    skillDetailsViewModelFactory: SkillDetailsViewModel.Factory
 ) {
-    //Put inside scaffold?
     NavHost(
         navController = navController,
         startDestination = MainScreen.Dashboard.route
@@ -29,9 +31,17 @@ fun SetupNavGraph(
             val selectedSkillId =
                 backStackEntry.arguments?.getString("skill_id")?.toInt() ?: -1
 
+            val skillDetailsViewModel = viewModel<SkillDetailsViewModel>(
+                factory = SkillDetailsViewModel.provideExerciseViewModelFactory(
+                    factory = skillDetailsViewModelFactory,
+                    skillId = selectedSkillId
+                )
+            )
+
             SkillDetailsScreen(
                 selectedSkillId = selectedSkillId,
-                navController = navController
+                navController = navController,
+                skillDetailsViewModel = skillDetailsViewModel
             )
         }
         composable(MainScreen.Search.route) { SearchScreen(navController) }
