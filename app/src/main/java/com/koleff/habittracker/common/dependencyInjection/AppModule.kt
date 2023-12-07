53e8eebdbf272ff3f32c001234aa0739bf678d6a
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-07 22:38:34 +0200

    Providing SkillListViewModel. Imports cleanup.
    This view model does not require assistInject.

--- app/src/main/java/com/koleff/habittracker/common/dependencyInjection/AppModule.kt
+++ app/src/main/java/com/koleff/habittracker/common/dependencyInjection/AppModule.kt
@@ -2,14 +2,13 @@ package com.koleff.habittracker.common.dependencyInjection
 
 import com.koleff.habittracker.data.repository.FakeSkillRepositoryImpl
 import com.koleff.habittracker.domain.repository.SkillRepository
-import com.koleff.habittracker.ui.viewModel.SkillDetailsViewModel
 import com.koleff.habittracker.ui.viewModel.SkillListViewModel
-import dagger.Component.Factory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -19,5 +18,11 @@ object AppModule {
     @Singleton
     fun provideFakeSkillRepositoryImpl(): SkillRepository {
         return FakeSkillRepositoryImpl()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSkillListViewModel(skillRepository: SkillRepository): SkillListViewModel {
+        return SkillListViewModel(skillRepository)
     }
 }
--- app/src/main/java/com/koleff/habittracker/ui/viewModel/SkillListViewModel.kt
+++ app/src/main/java/com/koleff/habittracker/ui/viewModel/SkillListViewModel.kt
@@ -2,18 +2,13 @@ package com.koleff.habittracker.ui.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.koleff.habittracker.R
 import com.koleff.habittracker.data.Skill
-import com.koleff.habittracker.data.SkillCategory
-import com.koleff.habittracker.data.SkillType
 import com.koleff.habittracker.domain.repository.SkillRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
